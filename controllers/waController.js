3c269b70c56509e21f87927a36a679d35f374ad5
Nasiex <officialrpl.smkufa@gmail.com>
2022-10-23 13:47:34 +0700

    up

--- controllers/waController.js
+++ controllers/waController.js
@@ -3,8 +3,11 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    headless: true,
-    args: [ '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process' ]
+    puppeteer: {
+        args: [
+            '--no-sandbox',
+        ],
+    },
 });
  
 client.on('qr', (qr) => {
